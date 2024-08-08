b62885c8d3e0f47c58f0d2f41eec84a4e69d596e
Fletcher <57144802+Fletcher-1116@users.noreply.github.com>
2024-08-08 17:56:48 +0800

    11

--- django_HKUST/app01/views/view_qa.py
+++ django_HKUST/app01/views/view_qa.py
@@ -1,9 +1,9 @@
 import json
-from app01.views import graph_rag
-from langchain_community.embeddings import HuggingFaceEmbeddings
-import requests
+
 from django.shortcuts import HttpResponse, render
 from django.views.decorators.csrf import csrf_exempt
+
+from app01.views import graph_rag
 
 # Create your views here.
 OPENAI_API_KEY = 'sk-YoZZU2QvKG6Kzqhv9c18648062D94c08965128Ce4196E2E8'
@@ -18,7 +18,7 @@ def chat(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         user_input = data['message']
-        
+
         # QA提示模版
         answer_template = """Answer the question based only on the following context:
                     {context}
@@ -29,18 +29,19 @@ def chat(request):
         print(answer_prompt)
         # QA链
         chain = (
-            graph_rag.RunnableParallel({
-                "context": graph_rag._search_query | graph_rag.retriever,
-                "question": graph_rag.RunnablePassthrough(),
-            })
-            | answer_prompt
-            | graph_rag.ChatOpenAI(temperature=0)
-            | graph_rag.StrOutputParser()
+                graph_rag.RunnableParallel({
+                    "context": graph_rag._search_query | graph_rag.retriever,
+                    "question": graph_rag.RunnablePassthrough(),
+                })
+                | answer_prompt
+                | graph_rag.ChatOpenAI(temperature=0)
+                | graph_rag.StrOutputParser()
         )
 
         # 运行QA链
-        output = chain.invoke({"question": user_input.lower()})
-        with open('app01/datasets/test.json','r',encoding='utf-8') as f:
+        # output = chain.invoke({"question": user_input.lower()})
+        output = '1111111'
+        with open('app01/datasets/test.json', 'r', encoding='utf-8') as f:
             paper_entity = json.load(f)
         # ans = chain.invoke(
         #     {
@@ -61,4 +62,4 @@ def chat(request):
         #                          stream=True)
 
         # res = response.json()['choices'][0]['message']['content']
-        return HttpResponse(json.dumps({'response': output,'entity': paper_entity}))
+        return HttpResponse(json.dumps({'response': output, 'entity': paper_entity}))
