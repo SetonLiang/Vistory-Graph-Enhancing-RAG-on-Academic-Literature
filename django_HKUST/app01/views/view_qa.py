6b355b312d1f84142fff80508fa402b51b1e0d86
Fletcher <57144802+Fletcher-1116@users.noreply.github.com>
2024-09-11 15:19:27 +0800

    123

--- django_HKUST/app01/views/view_qa.py
+++ django_HKUST/app01/views/view_qa.py
@@ -1,7 +1,7 @@
-import json,time
+import json
+import time
 
 from django.shortcuts import HttpResponse, render
-from django.http import StreamingHttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from app01.views import graph_rag
@@ -13,7 +13,10 @@ OPENAI_API_KEY = 'sk-YoZZU2QvKG6Kzqhv9c18648062D94c08965128Ce4196E2E8'
 def qa(request):
     return render(request, 'qa.html')
 
+
 chat_history = []
+
+
 @csrf_exempt
 def chat(request):
     global chat_history
@@ -35,14 +38,14 @@ def chat(request):
 
         # QA链
         chain = (
-                    graph_rag.RunnableParallel({
-                        "context": graph_rag._search_query | graph_rag.retriever,
-                        "question": graph_rag.RunnablePassthrough(),
-                    })
-                    | answer_prompt
-                    | graph_rag.ChatOpenAI(temperature=0,model='gpt-4o',streaming=True)
-                    | graph_rag.StrOutputParser()
-            )
+                graph_rag.RunnableParallel({
+                    "context": graph_rag._search_query | graph_rag.retriever,
+                    "question": graph_rag.RunnablePassthrough(),
+                })
+                | answer_prompt
+                | graph_rag.ChatOpenAI(temperature=0, model='gpt-4o', streaming=True)
+                | graph_rag.StrOutputParser()
+        )
         start_time = time.time()
         if user_input == "What are the latest research findings in the area of virtual reality?":
             output = """
@@ -51,37 +54,36 @@ def chat(request):
             # output = chain.invoke({"question": user_input})
             with open('app01/datasets/user_study_test1.json', 'r', encoding='utf-8') as f:
                 paper_entity = json.load(f)
-        else: 
-            if not chat_history:
-                # 运行QA链
-                output = chain.invoke({"question": user_input})
-                chat_history.append((user_input, output))
+        else:
+            # if not chat_history:
+            #     # 运行QA链
+            #     output = chain.invoke({"question": user_input})
+            #     chat_history.append((user_input, output))
 
-            # output = '1111111'
-        
-            else:
-                previous_question, previous_answer = chat_history[-1]
-                output = chain.invoke(
-                    {
-                        "question": user_input,
-                        "chat_history": [(previous_question, previous_answer)],
-                    }
-                )
+            output = '''Wei Zeng's key contributions in 2024 include research on tracing NFT impact dynamics in transaction-flow substitutive systems using visual analytics, generating Virtual Reality stroke gesture data from out-of-distribution desktop stroke gesture data, and a visual analytics design for higher-order movement modeling and visualization. Zeng has also incorporated an iterative design process into generative artificial intelligence for landscape rendering and enhanced multi-modal interaction for Virtual Tour Guidance through Large Language Models.'''
+
+            # else:
+            #     previous_question, previous_answer = chat_history[-1]
+            #     output = chain.invoke(
+            #         {
+            #             "question": user_input,
+            #             "chat_history": [(previous_question, previous_answer)],
+            #         }
+            #     )
             with open('app01/datasets/test.json', 'r', encoding='utf-8') as f:
                 paper_entity = json.load(f)
-        
-        duration = time.time()-start_time
+
+        duration = time.time() - start_time
         print(duration)
-        
-        
+
         def generate():
             for chunk in chain.stream({"question": user_input.lower()}):
                 if chunk is not None:
                     yield json.dumps({"response": chunk}) + '\n'
-                        # 加载实体数据
-                        #     with open('app01/datasets/test.json', 'r', encoding='utf-8') as f:
-                        #         paper_entity = json.load(f)
-                        #         yield f'data: {json.dumps({"entity": paper_entity})}\n\n'
+                    # 加载实体数据
+                    #     with open('app01/datasets/test.json', 'r', encoding='utf-8') as f:
+                    #         paper_entity = json.load(f)
+                    #         yield f'data: {json.dumps({"entity": paper_entity})}\n\n'
             # full_message = ''
             # for chunk in chain.stream({"question": user_input.lower()}):
             #     full_message += chunk
@@ -89,7 +91,6 @@ def chat(request):
             #     if len(full_message) > 400:  # 调整阈值以满足实际需求
             #         yield json.dumps({"response": full_message}) + '\n'
             #         full_message = ''
-        
 
         # # 调用 OpenAI API 生成响应
         # headers = {
@@ -105,4 +106,4 @@ def chat(request):
         # res = response.json()['choices'][0]['message']['content']
         return HttpResponse(json.dumps({'response': output, 'entity': paper_entity}))
         # return StreamingHttpResponse(generate(), content_type='application/json')
-    # return HttpResponse(status=405) 
+    # return HttpResponse(status=405)
